d1ba3a886df58b034b42bbd2e1aa785cc19712e5
nambt <nambt2k2@gmail.com>
2025-04-28 23:40:15 +0700

    import asset and learn shader graph

---
+++ Assets/Plugins/pointB_dev/ImageExtension/Screenshot.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class Screenshot {
+    [MenuItem("Tools/Image/Screenshot")]
+    public static void TakeScreenshotAsync() {
+        string savePath = GetSavePath();
+        if (string.IsNullOrEmpty(savePath)) {
+            Debug.Log("Screenshot cancelled");
+            return;
+        }
+
+        // Ensure directory exists
+        string directory = Path.GetDirectoryName(savePath);
+        if (!Directory.Exists(directory)) {
+            Directory.CreateDirectory(directory);
+        }
+
+        // Capture and save screenshot
+        ScreenCapture.CaptureScreenshot(savePath);
+        Debug.Log("Screenshot saved at: " + savePath);
+    }
+
+    static string GetSavePath() {
+        string defaultFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+        string defaultName = "Screenshot_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        string savePath = "";
+        savePath = EditorUtility.SaveFilePanel("Save Screenshot", defaultFolder, defaultName, "png");
+        return savePath;
+    }
+}
+#endif
---
+++ Assets/Plugins/pointB_dev/ReferenceExtension/PropertyCopier.cs
@@ -0,0 +1,66 @@
+﻿#if UNITY_EDITOR
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MonoBehaviour), true)]
+public class PropertyCopier : Editor {
+    static string name_selectProperty;
+    static object obj_copy;
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+        SerializedProperty property = serializedObject.GetIterator();
+        property.NextVisible(true);
+        while (property.NextVisible(true)) {
+            Rect propertyRect = EditorGUILayout.GetControlRect();
+            EditorGUI.PropertyField(propertyRect, property, true);
+            if (Event.current.type == EventType.MouseDown && propertyRect.Contains(Event.current.mousePosition)) {
+                name_selectProperty = property.name;
+            }
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    [MenuItem("Tools/Reference/Property Copier/Copy")]
+    static void CopyProperty() {
+        Component target = Selection.activeGameObject.GetComponent<MonoBehaviour>();
+        if (target == null)
+            return;
+        FieldInfo[] fields = target.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        foreach (FieldInfo field in fields) {
+            if (field.Name == name_selectProperty && typeof(Object).IsAssignableFrom(field.FieldType)) {
+                obj_copy = field.GetValue(target);
+                break;
+            }
+        }
+    }
+
+    [MenuItem("Tools/Reference/Property Copier/Paste")]
+    static void PasteProperty() {
+        Component target = Selection.activeGameObject.GetComponent<MonoBehaviour>();
+        if (target == null)
+            return;
+
+        FieldInfo[] fields = target.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        foreach (FieldInfo field in fields) {
+            if (field.Name == name_selectProperty && typeof(Object).IsAssignableFrom(field.FieldType)) {
+                object convertedValue = obj_copy;
+                if (typeof(Component).IsAssignableFrom(convertedValue.GetType()))
+                    convertedValue = (convertedValue as Component).gameObject;
+                if (typeof(Component).IsAssignableFrom(field.FieldType)) {
+                    Component componentCopy = obj_copy as Component;
+                    if (componentCopy != null)
+                        convertedValue = (obj_copy as Component).GetComponent(field.FieldType);
+                    else
+                        convertedValue = ((GameObject)obj_copy).GetComponent(field.FieldType);
+                }
+                if (convertedValue != null) {
+                    field.SetValue(target, convertedValue);
+                    break;
+                }
+            }
+        }
+    }
+}
+#endif
